fe71a5249e4be825a05db06b3e90aed126a56fe0
Marco <Marco@MDT>
2015-10-31 12:24:56 +0100

    search bar

--- src/java/servlets/ContextInitializer.java
+++ src/java/servlets/ContextInitializer.java
@@ -6,17 +6,9 @@
 package servlets;
 
 import classes.Database;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 /**
  *

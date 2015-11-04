c790c9ebab756a1cdff38cf57320c522260da55a
Gianlufil <filgianluca93@gmail.com>
2015-11-04 19:19:25 +0100

    Aggiunto slide tiles profilo

--- web/template/js/myscript.js
+++ web/template/js/myscript.js
@@ -14,5 +14,30 @@ $(document).ready(function(){
        $(".search-filters input, .search-filters select").val('');
        
     });
+    
+    var slide_lock = false;
+    
+    $(".tile-label a").click(function(e){
+        e.preventDefault();
+        if(!slide_lock){
+            slide_lock = true;
+            
+            var label = $(this);
+            var tile = label.parent().next();
+            
+            tile.slideToggle(function(){
+                
+                if(label.hasClass("collapsed")){
+                    label.removeClass("collapsed");
+                    label.children("i").removeClass("fa-chevron-right").addClass("fa-chevron-down");
+                } else {
+                    label.addClass("collapsed");
+                    label.children("i").removeClass("fa-chevron-down").addClass("fa-chevron-right"); 
+                }
+            });
+            
+            slide_lock = false;
+        }
+    });
         
 });

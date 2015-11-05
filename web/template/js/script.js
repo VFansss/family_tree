8f59a4a0790a05ce3f020f0b3d6e7b3f02aafaf8
Marco <Marco@MDT>
2015-11-05 01:45:50 +0100

    js login e profile

--- web/template/js/script.js
+++ web/template/js/script.js
@@ -5,29 +5,43 @@
  */
 
 $(document).ready(function(){
+    
     //Attivazione tooltips
     $('[data-toggle="tooltip"]').tooltip();
     
+    // Quando si preme il pulsante di reset
     $("#search-reset-filters").click(function(e){
-       e.preventDefault();
-       
-       $(".search-filters input, .search-filters select").val('');
-       
+        // Non eseguire il comportamente di default del submit
+        e.preventDefault();
+        // Svuota tutti i campi del form
+        $(".search-filters input, .search-filters select").val('');
     });
     
-    var slide_lock = false;
     
+    // Abilita lo slider dei tile
+    var slide_enabled = true;
+    
+    // Al click della label del tile
     $(".tile-label p:not(.no-collapsed)").click(function(e){
-        if(!slide_lock){
-            slide_lock = true;
+        // Se lo slider è abilitato
+        if(slide_enabled){
+            // Disabilita slider
+            slide_enabled = false;
             
+            // Caching degli elementi da utilizzare
             var label = $(this);
             var tile = label.parent().next();
             
-            tile.slideToggle(600, function(){});
+            // Esegui lo slider
+            tile.slideToggle(600, function(){
+                // Al termine, riabilita lo slider
+                slide_enabled = true;
+            });
             
+            // Esegui animazione della icona della label
             label.find("i.fa-chevron-down").toggleClass("down");
-            slide_lock = false;
+            
         }
+        
     });
 });

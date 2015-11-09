68bb5c526a2907d96e9ac3995107d0e5cf6b3ef9
Marco <Marco@MDT>
2015-11-09 02:41:48 +0100

    implementazione cleaner breadcrumb 2.0

--- src/java/it/collaborative_genealogy/tree/NodeList.java
+++ src/java/it/collaborative_genealogy/tree/NodeList.java
@@ -33,71 +33,58 @@ public class NodeList extends ArrayList<TreeNode>{
     
     public void cleaner(){
         // Iteratore principale
-        Iterator iter = this.iterator();
-        // Iteratore di supporto per eliminare gli utenti intermedi
-        Iterator iter_delete;
+        ListIterator iter = this.listIterator(0);
         // Iteratore inverso
         ListIterator iterator_reverse;
-        User user;
+        // Oggetti di supporto
+        User user,user_to_check, user_to_delete;
         UserList family_core;
-        // Lista che contiene gli utenti già controllati
-        UserList user_checked = new UserList();
         // Flag per controllare se è stato eliminato qualche utente
         boolean deleted = false;
-
         // Per ogni utente ({user}) nella breadcrumb (dal primo all'ultimo inserito)
         while(iter.hasNext()){
             user = ((TreeNode) iter.next()).getUser();
-            // Se è stato già verficato, passo all'utente successivo
-            if(user_checked.contains(user)) continue;
+            // Se {user} è stato già verficato, passa all'utente successivo
             try {
-                // Recupera il nucleo familiare dell'utente
+                // Recupera il nucleo familiare di {user}
                 family_core = user.getFamilyCore();
                 // Imposta iteratore inverso
                 iterator_reverse = this.listIterator(this.size());
                 // Per ogni utente ({user_to_check}) della breadcrumb (dall'ulitmo al primo inserito)
                 while(iterator_reverse.hasPrevious()) {
-                    User user_to_check = ((TreeNode) iterator_reverse.previous()).getUser();
-                    // Se {user_to_check} è uguale a {user}, esci dal ciclo
+                    user_to_check = ((TreeNode) iterator_reverse.previous()).getUser();
+                    // Se {user_to_check} è uguale a {user}, esci dal ciclo perchè sono stati verificati tutti gli utenti potenzialmente rimuovibili
                     if(user.equals(user_to_check)) break;
                     // Se {user_to_check} fa parte del nucleo familiare di {user} 
                     if(family_core.contains(user_to_check)){
-                        // Imposta iteratore di supporto
-                        iter_delete = this.iterator();
-                        // Porta l'iteratore di supporto all'utente puntato dall'iteratore principale
-                        while(iter_delete.hasNext()){
-                            if(user.equals(((TreeNode) iter_delete.next()).getUser())) break;                                        
-                        }
+          
                         // Per ogni utente da eliminare
-                        while(iter_delete.hasNext()){
-                            User user_to_delete = ((TreeNode) iter_delete.next()).getUser();
+                        while(iter.hasNext()){
+                            user_to_delete = ((TreeNode) iter.next()).getUser();
                             // Se non è uguale a {user_to_check}
                             if(!user_to_delete.equals(user_to_check)){
                                 // Rimuovilo
-                                iter_delete.remove();
+                                iter.remove();
                                 // Segnala che è stato eliminato un utente
                                 deleted = true;
 
                             }else{
-                                // Altrimenti, esci dall'iteratore di eliminazione per non eliminare gli utenti successivi
+                                 // Altrimenti, torna all'utente precedente ed esci dall'iteratore di eliminazione per non eliminare gli utenti successivi
+                                iter.previous();
                                 break;
                             }
                         }
-
+                        // Se è stato eliminato un utente
+                        if(deleted){
+                            deleted = false;
+                            // Passa a controllare l'utente successivo
+                            break;
+                        }
                     }
-                    // Se è stato eliminato un utente
-                    if(deleted){
-                        // Reinizializza l'iteratore prinpipale
-                        deleted = false;
-                        iter = this.iterator();
-                        break;
-                    }
+                    
                 }
 
-
             } catch (SQLException ex) { }
-            // Aggiungi {user} nella lista degli utenti controllati
-            user_checked.add(user);
         }
     }
 
